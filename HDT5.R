84cb45111b3a013666a505ffae116e1d36cc8b5c
RandyVen <35413122+RandyVen@users.noreply.github.com>
2021-03-28 19:17:10 -0600

    Clasificacion por Clustering / Grafico de codo

--- HDT5.R
+++ HDT5.R
@@ -38,3 +38,48 @@ set.seed(123)
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,]
 test<-datos[-corte,]
+
+# Revisamos el tipo de datos del conjunto de entrenamiento
+head(train)
+
+# Revisamos el tipo de datos del conjunto de test
+head(test)
+
+glimpse(train[1:10,])
+scatter.smooth(train$LotFrontage, train$SalePrice)
+scatter.smooth(train$LotArea, train$SalePrice)
+scatter.smooth(train$GrLivArea, train$SalePrice)
+scatter.smooth(train$YearBuilt, train$SalePrice)
+scatter.smooth(train$BsmtUnfSF, train$SalePrice)
+scatter.smooth(train$TotalBsmtSF, train$SalePrice)
+scatter.smooth(train$X1stFlrSF, train$SalePrice)
+scatter.smooth(train$GarageYrBlt, train$SalePrice)
+scatter.smooth(train$GarageArea, train$SalePrice)
+scatter.smooth(train$YearRemodAdd, train$SalePrice)
+scatter.smooth(train$TotRmsAbvGrd, train$SalePrice)
+scatter.smooth(train$MoSold, train$SalePrice)
+scatter.smooth(train$OverallQual, train$SalePrice)
+
+## Clasificacion por Clustering 
+datos <- train[,c("LotFrontage","LotArea","GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","X1stFlrSF","GarageYrBlt","GarageArea","YearRemodAdd", "SalePrice")]
+
+datos <- na.omit(datos)
+
+# Grafico de codo 
+wss <- (nrow(datos)-1)*sum(apply(datos,2,var))
+
+for (i in 2:10) 
+  wss[i] <- sum(kmeans(datos, centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
+
+# Aplicacion del algoritmo K-Means
+km<-kmeans(datos,3)
+datos$grupo<-km$cluster
+
+g1<- datos[datos$grupo==1,]
+prop.table(table(g1$SalePrice))*100
+g2<- datos[datos$grupo==2,]
+prop.table(table(g2$SalePrice))*100
+g3<- datos[datos$grupo==3,]
+prop.table(table(g3$SalePrice))*100
